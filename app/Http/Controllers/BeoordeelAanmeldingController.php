4cc24892269f183081a503a1c6dd0ed695391663
FriendlyRob <robvandheijden@gmail.com>
2017-05-15 13:12:48 +0000

    yoyo

--- app/Http/Controllers/BeoordeelAanmeldingController.php
+++ app/Http/Controllers/BeoordeelAanmeldingController.php
@@ -18,14 +18,14 @@ class BeoordeelAanmeldingController extends Controller
 {
     public function postacceptaanmelding(Request $request)
     {
-        $aanmeldingsId = DB::table('aanmeldings')->where('idAanmelding', $request["aanmelding"])->first();
+        $aanmeldingsId = DB::table('aanmeldings')->where('id', $request["aanmelding"])->first();
         DB::table('slots')->where('id', $aanmeldingsId->idSlot)->update(['idStatus' => 3]);
         $kosten = $aanmeldingsId->kosten;
         $oudBudget = DB::table('sprekerskostens')->first();
         $nieuwBudget = $oudBudget->budget - $kosten;
         DB::table('sprekerskostens')->where('id', 1)->update(['budget' => $nieuwBudget]);
         
-        DB::table('aanmeldings')->where('idAanmelding', $request["aanmelding"])->update(['status' => "Geaccepteerd"]);
+        DB::table('aanmeldings')->where('id', $request["aanmelding"])->update(['status' => "Geaccepteerd"]);
         
         $idSlot = DB::table('slots')->where('id', $aanmeldingsId->idSlot)->first();
         
@@ -89,17 +89,17 @@ class BeoordeelAanmeldingController extends Controller
     
     public function postAcceptTegenbod(Request $request)
     {
-        $aanmelding = DB::table('aanmeldings')->where('idAanmelding', $request["aanmeldingsId"])->first();
+        $aanmelding = DB::table('aanmeldings')->where('id', $request["aanmeldingsId"])->first();
         
-        DB::table('aanmeldings')->where('idAanmelding', $request["aanmeldingsId"])->update(['status' => "Geaccepteerd"]);
-        DB::table('aanmeldings')->where('idAanmelding', $request["aanmeldingsId"])->update(['kosten' => $aanmelding->tegenBod]);
+        DB::table('aanmeldings')->where('id', $request["aanmeldingsId"])->update(['status' => "Geaccepteerd"]);
+        DB::table('aanmeldings')->where('id', $request["aanmeldingsId"])->update(['kosten' => $aanmelding->tegenBod]);
         
         $kosten = $aanmelding->tegenBod;
         $oudBudget = DB::table('sprekerskostens')->first();
         $nieuwBudget = $oudBudget->budget - $kosten;
         DB::table('sprekerskostens')->where('id', 1)->update(['budget' => $nieuwBudget]);
         // Verander status van slot naar 3 (bezet)
-        $aanmelding = DB::table('aanmeldings')->where('idAanmelding', $request["aanmeldingsId"])->first();
+        $aanmelding = DB::table('aanmeldings')->where('id', $request["aanmeldingsId"])->first();
         DB::table('slots')->where('id', $aanmelding->idSlot)->update(['idStatus' => 3]);
         
         return redirect()->route("aanmelden")->with(["success" => "U heeft succesvol uw aanmelding geaccepteerd, voor een nieuwe prijs!"]);
@@ -107,11 +107,11 @@ class BeoordeelAanmeldingController extends Controller
     
     public function postWeigerTegenbod(Request $request)
     {
-        $aanmelding = DB::table('aanmeldings')->where('idAanmelding', $request["aanmeldingsId"])->first();
+        $aanmelding = DB::table('aanmeldings')->where('id', $request["aanmeldingsId"])->first();
         $slotId = $aanmelding->idSlot;
         
         DB::table('slots')->where('id', $slotId)->update(['idStatus' => 1]);
-        DB::table('aanmeldings')->where('idAanmelding', $request["aanmeldingsId"])->delete();
+        DB::table('aanmeldings')->where('id', $request["aanmeldingsId"])->delete();
         
         return redirect()->route("aanmelden")->with(["success" => "U heeft succesvol uw aanmelding geweigerd!"]);
     }
