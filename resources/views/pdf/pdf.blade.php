c097796b98f42902b2ba20cd6f2750e5e43bee84
FriendlyRob <robvandheijden@gmail.com>
2017-05-16 06:54:22 +0000

    Fix Qr code pdf mail

--- resources/views/pdf/pdf.blade.php
+++ resources/views/pdf/pdf.blade.php
@@ -4,7 +4,6 @@ Hier zijn uw tickets:<br>
 <br>
 @foreach($ticketarray as $ticket)
     <h5>Ticket</h5>
-    <img src="http://conferentie-site-bunky.c9users.io/src/tickets/{{ $ticket->id }}.jpg"><br>
     <img src="http://damp-reef-29348.herokuapp.com/src/tickets/{{ $ticket->id }}.jpg"><br>
 @endforeach
 <br>
@@ -12,5 +11,5 @@ Hier zijn uw maaltijden:<br>
 <br>
 @foreach($maaltijdarray as $maaltijd)
     <h5>Maaltijd</h5>
-    <img src="http://conferentie-site-bunky.c9users.io/src/maaltijden/{{ $maaltijd->id }}.jpg"><br>
+    <img src="http://damp-reef-29348.herokuapp.com/src/maaltijden/{{ $maaltijd->id }}.jpg"><br>
 @endforeach
